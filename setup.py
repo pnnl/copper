905ffcedd727581598463bfe6165a4171237f5bb
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2022-05-12 22:45:55 -0700

    update metadata.

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name="copper",
     version="0.0.1",
+    description="Performance curves generator for building energy simulation.",
+    keywords="building energy simulation performance curves",
+    url="https://github.com/pnnl/copper",
+    author="Jérémy Lerond",
+    author_email="jeremy.lerond@pnnl.gov",
     packages=find_packages(),
     install_requires=[
         "numpy",
