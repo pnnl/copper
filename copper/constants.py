3c2aef37fd3a07c93ded2952eb302ca10f954f8a
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2023-10-27 08:54:52 -0700

    Fix path issue.

--- copper/constants.py
+++ copper/constants.py
@@ -5,15 +5,12 @@ Holds all the constants referenced in copper.
 """
 
 import os
-from pathlib import Path
 
 LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-filep = Path(__file__)
-CHILLER_SCHEMA_PATH = os.path.join(filep, "/schema/", "copper.chiller.schema.json")
-CHILLER_GENE_SCHEMA_PATH = os.path.join(
-    filep, "/schema/", "copper.chiller.generate_set_of_curves.schema.json"
+ROOT_DIR = os.path.dirname(__file__)
+CHILLER_SCHEMA_PATH = f"{ROOT_DIR}/../schema/copper.chiller.schema.json"
+CHILLER_GENE_SCHEMA_PATH = (
+    f"{ROOT_DIR}/../schema/copper.chiller.generate_set_of_curves.schema.json"
 )
-CHILLER_ACTION_SCHEMA_PATH = os.path.join(
-    filep, "/schema/", "copper.chiller.action.schema.json"
-)
-SCHEMA_PATH = os.path.join(filep, "/schema/", "copper.schema.json")
+CHILLER_ACTION_SCHEMA_PATH = f"{ROOT_DIR}/../schema/copper.chiller.action.schema.json"
+SCHEMA_PATH = f"{ROOT_DIR}/../schema/copper.schema.json"
