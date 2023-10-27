0dfb38c75373b72cc5f9026064a73b8566bbf1e6
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2023-10-26 21:41:42 -0700

    Test.

--- copper/constants.py
+++ copper/constants.py
@@ -5,13 +5,15 @@ Holds all the constants referenced in copper.
 """
 
 import os
+from pathlib import Path
 
 LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
-CHILLER_SCHEMA_PATH = os.path.join("./schema/", "copper.chiller.schema.json")
+filep = Path(__file__)
+CHILLER_SCHEMA_PATH = os.path.join(filep, "/schema/", "copper.chiller.schema.json")
 CHILLER_GENE_SCHEMA_PATH = os.path.join(
-    "./schema/", "copper.chiller.generate_set_of_curves.schema.json"
+    filep, "/schema/", "copper.chiller.generate_set_of_curves.schema.json"
 )
 CHILLER_ACTION_SCHEMA_PATH = os.path.join(
-    "./schema/", "copper.chiller.action.schema.json"
+    filep, "/schema/", "copper.chiller.action.schema.json"
 )
-SCHEMA_PATH = os.path.join("./schema/", "copper.schema.json")
+SCHEMA_PATH = os.path.join(filep, "/schema/", "copper.schema.json")
