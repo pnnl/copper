0f04b8f9dc59e48f5b566792aa18b4621000caf6
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2023-11-16 13:42:31 -0800

    Increment release.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Jeremy Lerond'
 # The short X.Y version
 version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.1.5'
+release = '0.1.9'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 setup(
     name="copper-bem",
-    version="0.1.6",
+    version="0.1.9",
     description="Performance curve generator for building energy simulation",
     long_description=long_description,
     long_description_content_type="text/markdown",
