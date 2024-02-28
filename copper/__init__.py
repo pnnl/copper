f1b451b04d353ecfd08eb368fecdaa2b62568497
Wan <hanlong.wan@pnnl.gov>
2024-02-28 11:52:40 -0800

    update generator again

--- copper/__init__.py
+++ copper/__init__.py
@@ -1,8 +1,8 @@
 from copper.chiller import *
+from copper.generator import *
 from copper.schema import *
 from copper.unitary_dx import *
 from copper.constants import LOGGING_FORMAT
-from copper.generator import *
 import sys
 
 logging.basicConfig(format=LOGGING_FORMAT, stream=sys.stdout, level=logging.INFO)
