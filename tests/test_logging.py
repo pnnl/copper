437d7102a885120074deae26af5affd94a0bec9e
Lerond, Jeremy <jeremy.lerond@pnnl.gov>
2024-08-27 22:44:09 -0700

    Fix typo.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -35,7 +35,7 @@ class TestLogging(TestCase):
             == "Target not met after 1 generations; Restarting the generator."
         )
         self.assertTrue(captured[0][0].levelname == "WARNING")
-        self.assertTrue("GEN: 0, IPLV: 5.56, kW/ton: 5" in captured[0][1].msg)
+        self.assertTrue("GEN: 0, IPLV: 5.56, kW/ton: 5.2" in captured[0][1].msg)
         self.assertTrue(captured[0][1].levelname == "INFO")
         self.assertTrue(
             captured[0][2].msg
