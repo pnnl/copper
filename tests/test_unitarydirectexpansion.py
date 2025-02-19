b57b259ce2ecda332f99ff114c84ec313b38e23d
Jeremy L. <5149279+lymereJ@users.noreply.github.com>
2025-02-18 16:43:31 -0800

    Handle multiple sets of curves for different stages/speeds (#125)
    * Handle sets of curves for different stages/speeds.
    
    * Fix unit test failure.

--- tests/test_unitarydirectexpansion.py
+++ tests/test_unitarydirectexpansion.py
@@ -312,7 +312,7 @@ class UnitaryDirectExpansion(TestCase):
         self.dx_unit_dft.degradation_coefficient = 0
         self.dx_unit_dft.add_cycling_degradation_curve(overwrite=True)
         assert len(self.dx_unit_dft.set_of_curves) == 5
-        assert self.dx_unit_dft.get_dx_curves()["plf-f-plr"].coeff1 == 1.0
+        assert self.dx_unit_dft.get_dx_curves()["1"]["plf-f-plr"].coeff1 == 1.0
 
     def test_NN_wght_avg(self):
         # Define equipment
@@ -366,3 +366,10 @@ class UnitaryDirectExpansion(TestCase):
         assert round(set_of_curves[2].evaluate(1.0, 0), 2) == 1.0
         assert round(set_of_curves[3].evaluate(1.0, 0), 2) == 1.0
         assert round(set_of_curves[4].evaluate(1.0, 0), 2) == 1.0
+
+    def test_get_ms_curves(self):
+        new_curve = cp.Curve(eqp=self.dx_unit_dft, c_type="quad")
+        new_curve.speed = "2"
+        new_curve.out_var = "eir-f-t"
+        self.dx_unit_dft.set_of_curves.append(new_curve)
+        assert len(self.dx_unit_dft.get_dx_curves()["2"]) == 1
