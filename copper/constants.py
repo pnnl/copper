7f6473549c38d27aaf816e4fcf8b0c5e82a4a9ba
Jeremy L. <5149279+lymereJ@users.noreply.github.com>
2025-05-02 22:33:42 -0700

    Schema and CLI input validation for unitary DX equipment (#130)
    * Update version.
    
    * Update schema and CLI validation for unitary DX equipment.
    
    ---------
    
    Co-authored-by: jeremy.lerond@pnnl.gov <lero023@wf11081.pnl.gov>

--- copper/constants.py
+++ copper/constants.py
@@ -9,8 +9,15 @@ import os
 LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 ROOT_DIR = os.path.dirname(__file__)
 CHILLER_SCHEMA_PATH = f"{ROOT_DIR}/data/schema/copper.chiller.schema.json"
+UNITARYDIRECTEXPANSION_SCHEMA_PATH = (
+    f"{ROOT_DIR}/data/schema/copper.unitarydirectexpansion.schema.json"
+)
 CHILLER_GENE_SCHEMA_PATH = (
     f"{ROOT_DIR}/data/schema/copper.chiller.generate_set_of_curves.schema.json"
 )
+UNITARYDIRECTEXPANSION_GENE_SCHEMA_PATH = f"{ROOT_DIR}/data/schema/copper.unitarydirectexpansion.generate_set_of_curves.schema.json"
 CHILLER_ACTION_SCHEMA_PATH = f"{ROOT_DIR}/data/schema/copper.chiller.action.schema.json"
+UNITARYDIRECTEXPANSION_ACTION_SCHEMA_PATH = (
+    f"{ROOT_DIR}/data/schema/copper.unitarydirectexpansion.action.schema.json"
+)
 SCHEMA_PATH = f"{ROOT_DIR}/data/schema/copper.schema.json"
